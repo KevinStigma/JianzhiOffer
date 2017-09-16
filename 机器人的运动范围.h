e98a24e055bdb20569c15aa4ab477d01c0aa83d2
kevinstigma <781422447@qq.com>
2017-09-16 16:47:59 +0800

    good luck

--- 机器人的运动范围.h
+++ 机器人的运动范围.h
@@ -1,42 +1,52 @@
 class Solution {
 public:
+	int getDigitSum(int col,int row)
+	{
+		int sum=0;
+		while(col)
+		{
+			sum+=(col%10);
+			col=col/10;
+		}
+		while(row)
+		{
+			sum+=(row%10);
+			row=row/10;
+		}
+		return sum;
+	}
+	void search(vector<vector<int>>& visited,int cur_row,int cur_col,int threshold,int rows,int cols,int& count)
+	{
+		if(cur_row>=rows||cur_col>=cols||cur_row<0||cur_col<0)
+			return;
+		if(visited[cur_row][cur_col])
+			return;
+		int sum=getDigitSum(cur_col,cur_row);
+		if(sum>threshold)
+		{
+			visited[cur_row][cur_col]=1;
+			return;
+		}
+		count++;
+		visited[cur_row][cur_col]=1;
+		search(visited,cur_row+1,cur_col,threshold,rows,cols,count);
+		search(visited,cur_row-1,cur_col,threshold,rows,cols,count);
+		search(visited,cur_row,cur_col+1,threshold,rows,cols,count);
+		search(visited,cur_row,cur_col-1,threshold,rows,cols,count);
+	}
+	
     int movingCount(int threshold, int rows, int cols)
     {
         if(threshold<1 || rows<1 || cols<1)
             return 0;
-        else
-            {
-            bool* visited = new bool[rows*cols];
-            memset(visited,0,rows*cols);
-            int ncount =  movingCountcore(threshold,rows,cols,0,0,visited);
-            delete[] visited;
-            return ncount;
-        }
-             
-    }
-     
-    int movingCountcore(int threshold, int rows, int cols, int row, int col, bool* visited)
-    {
-        int ncount=0;
-        if(col>=0 && col<cols && row>=0 && row<rows && !visited[row*cols+col] && (getnum(col)+getnum(row))<=threshold)
-            {
-            visited[row*cols+col]=true;
-            ncount++;
-            ncount+=movingCountcore(threshold,rows,cols,row+1,col,visited);
-            ncount+=movingCountcore(threshold,rows,cols,row-1,col,visited);
-            ncount+=movingCountcore(threshold,rows,cols,row,col+1,visited);
-            ncount+=movingCountcore(threshold,rows,cols,row,col-1,visited);
-        }
-        return ncount;
-    }
-    int getnum(int num)
-        {
-        int nvalue=0;
-        while(num>0)
-            {
-            nvalue+=num%10;
-            num/=10;
-        }
-        return nvalue;
+		vector<vector<int>> visited(rows);
+		for(int i=0;i<rows;i++)
+		{
+			vector<int> v(cols);
+			visited[i].assign(v.begin(),v.end());
+		}
+		int count=0;
+		search(visited,0,0,threshold,rows,cols,count);
+		return count;
     }
 };
